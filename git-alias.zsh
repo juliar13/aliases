7b1b77609ee91c8aee3fef2a159926e5e814e67b
sho-yamasaki <judydoctrine@gmail.com>
2024-06-21 16:48:46 +0900

    git-alias added and refactoring

--- git-alias.zsh
+++ git-alias.zsh
@@ -1,20 +1,31 @@
 alias ga='git add'
+
 alias gb='git branch'
 alias gba='git branch -a'
 alias gbd='git branch -d'
+
 alias gcm='git commit -m'
+
 alias gd='git diff'
 alias gdc='git diff --cached'
+
 alias gfa='git fetch --all'
+
 alias gl='git log'
 alias gl1="git log --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso"
+
 alias grhh='git reset --hard HEAD'
 alias grsh='git reset --soft HEAD'
+
 alias grs='git restore --staged'
+
 alias grv='git remote -v'
+
 alias gs='git status'
+
 alias gsw='git switch'
 alias gsw-='git switch -'
 alias gswc='git switch -c'
-alias gtn='git tag -n'
 
+alias gtam='git tag -am'
+alias gtn='git tag -n'
