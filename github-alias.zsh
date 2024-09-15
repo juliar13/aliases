4e4c7c70320cdb34ba77eb1bc3708397915605c4
sho-yamasaki <judydoctrine@gmail.com>
2024-09-15 16:49:33 +0900

    create GitHub

---
+++ github-alias.zsh
@@ -0,0 +1,23 @@
+# gh auth
+alias gha='gh auth'
+
+# gh config
+alias ghc='gh config'
+
+# gh issue
+alias ghi='gh issue'
+
+# gh pr
+alias ghpr='gh pr'
+alias ghprc='gh pr create'
+alias ghprm='gh pr merge'
+
+# gh release
+alias ghr='gh release'
+
+# gh repo
+alias ghrepoc='gh repo create'
+
+# gh workflow
+alias ghwf='gh workflow'
+alias ghwfr='gh workflow run'
